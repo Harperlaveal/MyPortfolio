307e7866f41fc4f8bd25e24c152a67e91415ce1f
Harper Doak <doakharp@ecs.vuw.ac.nz>
2023-09-14 00:44:06 +1200

    Added hover for the navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -30,7 +30,7 @@ export default function Navbar() {
                     className={`${
                       pathname === "/"
                         ? "border-blue-500 dark:text-white h-full inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
-                        : "border-transparent text-gray-500 dark:text-gray-300 dark:hover:text-white inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+                        : "border-transparent text-gray-500 hover:text-black dark:text-gray-300 dark:hover:text-white inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
                     }`}
                   >
                     Home
@@ -41,7 +41,7 @@ export default function Navbar() {
                     className={`${
                       pathname === "/projects"
                         ? "border-blue-500 dark:text-white h-full inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
-                        : "border-transparent text-gray-500 dark:text-gray-300 dark:hover:text-white inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+                        : "border-transparent text-gray-500 hover:text-black dark:text-gray-300 dark:hover:text-white inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
                     }`}
                   >
                     Projects
@@ -52,7 +52,7 @@ export default function Navbar() {
                     className={`${
                       pathname === "/experience"
                         ? "border-blue-500 dark:text-white h-full inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
-                        : "border-transparent text-gray-500 dark:text-gray-300 dark:hover:text-white inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+                        : "border-transparent text-gray-500 hover:text-black dark:text-gray-300 dark:hover:text-white inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
                     }`}
                   >
                     Experience
