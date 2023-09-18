3924e4cfd07aa3a8572fc35b1004cfc44a0e89c6
Harper Doak <doakharp@ecs.vuw.ac.nz>
2023-09-18 15:19:10 +1200

    Add transitions n shit

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,40 @@
+"use client"
+
 import Image from 'next/image'
-import Navbar from './components/navbar'
+import { useState } from 'react'
 import Me from '@/public/HarperD.jpg'
+import MeHover from '@/public/HoverMe.png'
+
+
+
 
 export default function Home() {
+  const [imgSrc, setImgSrc] = useState(Me);
+  const [transitionClass, setTransitionClass] = useState('slide');
+
+  const handleMouseEnter = () => {
+    setTransitionClass('slide-out-left');
+    setTimeout(() => {
+    setImgSrc(MeHover);
+    setTransitionClass('slide');
+    }, 300); // this matches the transition time in the CSS
+  };
+  
+  const handleMouseLeave = () => {
+    setTransitionClass('slide-out-left');
+    setTimeout(() => {
+    setImgSrc(Me);
+    setTransitionClass('slide');
+    }, 300);
+  };
+
   return (
     <div>
       <div className="space-y-2 pt-5 pb-8 md:space-x-5">
         <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl
         sm:leading-10 md:text-6xl md:leading-13">
           Hello, I&apos;m{" "}
-          <span className="bg-gradient-to-r from-blue-400 via-purple-500 to-red-600
-          bg-clip-text text-transparent">
+          <span className="bg-gradient-to-r from-blue-400 via-purple-500 to-red-600 bg-clip-text text-transparent shimmer-effect">
             Harper
           </span>
           !
@@ -20,9 +44,13 @@ export default function Home() {
       <div className="border-t border-gray-200 dark:border-gray-700 items-center space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
         <div>
           <div className="flex flex-col items-center pt-8">
-            <Image alt="Picture of me" src={Me}
-              className="h-48 w-48 rounded-full object-cover object-top">
-            </Image>
+          <Image 
+          alt="Picture of me"
+          src={imgSrc}
+          className={`h-48 w-48 rounded-full object-cover object-top ${transitionClass}`}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+      />
             <h3 className="pt-4 pb-2 text-2xl font-bold leading-8 tracking-tight">Harper Doak</h3>
             <p className="text-gray-500 dark:text-gray-300 text-center">
               Software Developer
@@ -86,10 +114,10 @@ export default function Home() {
               </a>
               <a href="https://nextjs.org/" target="_blank">
                 <Image src="/nextjs.png" alt="Next.js Logo" width={50} height={50} />
-              </a>  
-              <Image src="/typescript.png" alt="TypeScript Logo" width={50} height={50} />         
+              </a>
+              <Image src="/typescript.png" alt="TypeScript Logo" width={50} height={50} />
               <Image src="/html.png" alt="Html Logo" width={50} height={50} />
-              <Image src="/css.png" alt="CSS Logo" width={50} height={50} />  
+              <Image src="/css.png" alt="CSS Logo" width={50} height={50} />
             </div>
           </div>
         </div>
