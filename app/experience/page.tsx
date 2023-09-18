8b75820b7b3d8f0e9a505e5871e394e275e0fa03
doakharp <doakharp@myvuw.ac.nz>
2023-09-18 16:49:50 +1200

    Sorted the experience by date

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -47,34 +47,35 @@ import { client } from "../lib/sanity";
                 </h1>
         </div>
         <div className="mb-10 border-t border-gray-200 dark:border-gray-700 items-center pt-4 flex flex-col gap-y-12">
-        {data.map((job) => (
-          <div
+    {data.sort((a, b) => new Date(b.endDate).getTime() - new Date(a.endDate).getTime()).map((job) => (
+        <div
             key={job._id}
             className="flex items-start lg:gap-x-6 gap-x-4 max-w-2xl relative before:absolute before:bottom-0 before:top-[4.5rem] before:left-7 before:w-[1px] before:h-[calc(100%-50px)] before:bg-zinc-800"
-          >
+        >
             <a
-              href={job.url}
-              rel="noreferrer noopener"
-              className="border border-black min-h-[60px] min-w-[60px] rounded-md overflow-clip relative"
+                href={job.url}
+                rel="noreferrer noopener"
+                className="border border-black min-h-[60px] min-w-[60px] rounded-md overflow-clip relative"
             >
-              <Image
-                src={job.logo}
-                className="object-cover"
-                alt={`${job.name} logo`}
-                fill
-              />
+                <Image
+                    src={job.logo}
+                    className="object-cover"
+                    alt={`${job.name} logo`}
+                    fill
+                />
             </a>
             <div className="flex flex-col items-start">
-              <h3 className="text-xl font-bold">{job.name}</h3>
-              <p>{job.jobTitle}</p>
-              <small className="text-sm text-zinc-500 mt-2 tracking-widest uppercase">
-                {job.startDate.toString()} - {job.endDate.toString()}
-              </small>
-              <p className="text-base text-black dark:text-white my-4">{job.description}</p>
+                <h3 className="text-xl font-bold">{job.name}</h3>
+                <p>{job.jobTitle}</p>
+                <small className="text-sm text-zinc-500 mt-2 tracking-widest uppercase">
+                    {job.startDate.toString()} - {job.endDate.toString()}
+                </small>
+                <p className="text-base text-black dark:text-white my-4">{job.description}</p>
             </div>
-          </div>
-        ))}
         </div>
+    ))}
+</div>
+
     </div>
     )
 }
